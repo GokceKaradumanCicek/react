4c5c861132f5397d7b99475ba1aada8899f757a9
Gokce Karaduman <gokce@thestarware.com>
2021-10-14 15:48:06 +0300

    Creating first component in react called ExpenseIten.js

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Hey, Gökçe Karaduman Çiçek!</h2> 
+      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
