387ca269781f2090813fec6688197edc5fa5ee87
Gokce Karaduman <gokce@thestarware.com>
2021-10-19 14:14:55 +0300

    Changing title when click on button(not on the screen)

--- react-complete-guide/src/components/Expense/ExpenseItem.js
+++ react-complete-guide/src/components/Expense/ExpenseItem.js
@@ -2,15 +2,17 @@ import Card from '../UI/Card.js';
 import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
+  let title=props.title;
   const clickHandler=()=>{
-   console.log("Clicked!!!");
+   title='Updated!!';
+   console.log(title);
   }
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change İtem</button>
