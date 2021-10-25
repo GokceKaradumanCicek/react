c8f5c6d4b8b79160d30c3d19377dc961d25d52dd
GokceKaradumanCicek <gokce@thestarware.com>
2021-10-25 23:28:34 +0300

    Adding props to ExpenseItem on App.js

--- expense_list/src/component/ExpenseItem.js
+++ expense_list/src/component/ExpenseItem.js
@@ -1,13 +1,20 @@
-import './ExpenseItem.css';
-const ExpenseItem=()=>{
- return(
-     <div className="expense-item">
-         <div>Date</div>
-         <div className="expense-item__description">
-             <h2>Title</h2>
-             <div className="expense-item__price">$76.8$</div>
-         </div>
-     </div>
- );
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className='expense-item'>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}$</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
