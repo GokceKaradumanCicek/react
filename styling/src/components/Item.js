e2de0ab69a8506bd383d68bb64a62f6ad67fb6bb
GokceKaradumanCicek <gokce@thestarware.com>
2021-10-26 12:02:31 +0300

    Course Goal label turns to red if nothing entered and submit

--- styling/src/components/Item.js
+++ styling/src/components/Item.js
@@ -3,17 +3,22 @@ import React, { useState } from "react";
 import Button from "../UI/Button";
 function Item(props) {
   const [enteredValue, setEnteredValue] = useState("");
+  const[isValid, setIsValid]=useState(true);
   const InputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault();
+    if(enteredValue.trim().length===0){
+        setIsValid(false);
+        return;
+    }
     props.onAddGoal(enteredValue);
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
+        <label  style={{color: !isValid? 'red': 'black' }}>Course Goal</label>
         <input type="text" onChange={InputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
