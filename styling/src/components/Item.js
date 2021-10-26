8121ce038cb77c18f2bcc16b1df34bb2ba0dc6d8
GokceKaradumanCicek <gokce@thestarware.com>
2021-10-26 16:35:29 +0300

    Second component 'FormComponent'for styling in the Item.js file

--- styling/src/components/Item.js
+++ styling/src/components/Item.js
@@ -1,6 +1,40 @@
-import "./Item.css";
 import React, { useState } from "react";
 import Button from "../UI/Button";
+import styled from 'styled-components';
+
+const FormControl=styled.div`
+    margin: 0.5rem 0;
+  
+  &:label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+  
+  &:input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+  
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+  &.invalid input{
+      border-color: red;
+      background: rgb(216, 89, 89);
+  }
+  .form-control.invalid label{
+  color:red;    
+}
+  
+`
+
 function Item(props) {
   const [enteredValue, setEnteredValue] = useState("");
   const[isValid, setIsValid]=useState(true);
@@ -20,10 +54,10 @@ function Item(props) {
   };
   return (
     <form onSubmit={submitHandler}>
-      <div className={`form-control ${!isValid? 'invalid': ''}`}>
-        <label>Course Goal</label>
+      <FormControl>
+                  <label>Course Goal</label>
         <input type="text" onChange={InputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
