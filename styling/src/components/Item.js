05e5174ef4257793c9ca9fb39f2e97c74b31d548
GokceKaradumanCicek <gokce@thestarware.com>
2021-10-27 15:36:53 +0300

    Wrong style name but class name is form-control

--- styling/src/components/Item.js
+++ styling/src/components/Item.js
@@ -1,33 +1,34 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
-import styled from "styled-components";
+// import styled from "styled-components";
+import style from "./Item.module.css";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props)=>(props.inValid ? 'red':'black')}
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props)=>(props.inValid ? 'red':'black')}
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.inValid? 'red' : '#ccc')};
-    background: #ffd7d7;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.inValid? 'red' : '#ccc')};
+//     background: #ffd7d7;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 function Item(props) {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
@@ -47,10 +48,10 @@ function Item(props) {
   };
   return (
     <form onSubmit={submitHandler}>
-      <FormControl inValid={!isValid}>
+      <div className={style.form-control}>
         <label>Course Goal</label>
         <input type="text" onChange={InputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
