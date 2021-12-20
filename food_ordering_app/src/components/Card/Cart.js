6d91fd3397f064513302f839eb5078eef090c7eb
GokceKaradumanCicek <gokce@thestarware.com>
2021-12-20 17:08:37 +0300

    Creating portal with Modal.js component and public index.html for Cart component, and render in App.js

--- food_ordering_app/src/components/Card/Cart.js
+++ food_ordering_app/src/components/Card/Cart.js
@@ -1,7 +1,8 @@
 import classes from './Cart.module.css';
+import Modal from '../UI/Modal';
 const Cart =()=>{
     const cardItems=<ul className={classes['cart-items']}>{ [{id: 'c1', name:'Sushi', amount:2, price:12.99}].map(item=>{<li>{item.name}</li>}) }</ul>;
-    return<div>
+    return<Modal>
          {cardItems}
         <div className={classes.total}>
             <span>Total Amount</span>
@@ -11,7 +12,6 @@ const Cart =()=>{
             <button className={classes['button--alt']}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
-    </div>
-
+    </Modal>
 }
 export default Cart;
