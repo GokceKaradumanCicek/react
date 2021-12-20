930046fafb667426f41b997594d149fa9e9b78eb
GokceKaradumanCicek <gokce@thestarware.com>
2021-12-20 15:22:32 +0300

    Creating custom Input and add to MealItemForm.js

---
+++ food_ordering_app/src/components/UI/Input.js
@@ -0,0 +1,8 @@
+import classes from './Input.module.css';
+const Input=(props)=>{
+    return <div className={classes.input}>
+        <label htmlFor={props.input.id} className={classes['input label']}>{props.label}</label>
+        <input {...props.input} className={classes['input input']} />
+    </div>
+}
+export default Input;
