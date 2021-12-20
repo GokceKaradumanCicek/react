e7d1f4886574a9a8823b4d45a6e68bcbd9e5b554
GokceKaradumanCicek <gokce@thestarware.com>
2021-12-20 10:16:39 +0300

    Avalibale meals,Meals summary and rendering both meals component

---
+++ food_ordering_app/src/components/Meals/MealsSummary.js
@@ -0,0 +1,10 @@
+import classes from './MealsSummary.module.css';
+const MealsSummary=()=>{
+    return( <section className={classes.summary}>
+        <h2 >Delicious Food,Delivered To You</h2>
+        <p>Choose your favourite meal and enjoy the delicious lunch or dinner at home</p>
+        <p>All our meals are cooked with high-quality ingredients, just-in-time and of coursse by experienced chefs!</p>
+    </section>
+    );  
+}
+export default MealsSummary;
