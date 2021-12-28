160c0b89fee2212dea9ab84379504ec230b197c3
GokceKaradumanCicek <gokce@thestarware.com>
2021-12-28 13:37:37 +0300

    useRef to reference html element(this is input in this example)

--- useref-project/src/App.js
+++ useref-project/src/App.js
@@ -2,15 +2,22 @@ import React,{useState,useEffect, useRef} from 'react';
 function App() {
   const [name, setName]=useState('');
   const renderedCount=useRef(0);
+  const inputRef=useRef();
 
   useEffect(()=>{
     renderedCount.current=renderedCount.current +1;
   })
+
+  function focus(){
+    console.log('!!!!!!!');
+    console.log(inputRef.current);
+  }
   return (
    <div>
-     <input value={name} onChange={(e)=>setName(e.target.value)} />
+     <input ref={inputRef} value={name} onChange={(e)=>setName(e.target.value)} />
      <div>My name is {name}</div>
      <div>I rendered {renderedCount.current} times.</div>
+     <button onClick={focus}>Focus</button>
    </div>
   );
 }
