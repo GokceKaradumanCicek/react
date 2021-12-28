a33efa9e6ea25914446d13eb7e57c905ecc09356
GokceKaradumanCicek <gokce@thestarware.com>
2021-12-28 13:31:31 +0300

    useRef to find how many this component renders using useEffect

--- useref-project/src/App.js
+++ useref-project/src/App.js
@@ -1,10 +1,16 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect, useRef} from 'react';
 function App() {
   const [name, setName]=useState('');
+  const renderedCount=useRef(0);
+
+  useEffect(()=>{
+    renderedCount.current=renderedCount.current +1;
+  })
   return (
    <div>
      <input value={name} onChange={(e)=>setName(e.target.value)} />
      <div>My name is {name}</div>
+     <div>I rendered {renderedCount.current} times.</div>
    </div>
   );
 }
