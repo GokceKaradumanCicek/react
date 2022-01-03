94a3cae6d0c3f79c74e27b182d8dccb972b4b289
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-03 16:52:16 +0300

    Adding basic validation for input is entered or not using enteredIsValidState

--- Forms-User-Input/src/components/SimpleInput.js
+++ Forms-User-Input/src/components/SimpleInput.js
@@ -1,10 +1,17 @@
 import React,{useRef, useState} from 'react';
 const SimpleInput = (props) => {
   const nameInputRef= useRef();
+  const[enteredNameIsValid, setEnteredNameIsValid]=useState(true);
+
 
   const formSubmitHandler= event=>{
     event.preventDefault();
     const enteredValue=nameInputRef.current.value;
+    if(enteredValue.trim()=== ''){
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
     console.log(enteredValue);
   }
   return (
@@ -12,6 +19,7 @@ const SimpleInput = (props) => {
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' />
+        {!enteredNameIsValid && <p className='error-text'>You did'nt enter any name,Please enter a name</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
