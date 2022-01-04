421bfea9cac1cde39766d4d41b4cf3774d6062b7
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-04 10:31:25 +0300

    Adding touching condition for input

--- Forms-User-Input/src/components/SimpleInput.js
+++ Forms-User-Input/src/components/SimpleInput.js
@@ -1,11 +1,19 @@
-import React,{useRef, useState} from 'react';
+import React,{useRef, useState, useEffect} from 'react';
 const SimpleInput = (props) => {
   const nameInputRef= useRef();
-  const[enteredNameIsValid, setEnteredNameIsValid]=useState(true);
+  const[enteredNameIsValid, setEnteredNameIsValid]=useState(false);
+  const[enteredNameTouched, setEnteredNameTouched]=useState(false);
+
+  useEffect(()=>{
+    if(enteredNameIsValid){
+      console.log("Hey, your input is valid!");
+    }
+  },[enteredNameIsValid]);
 
 
   const formSubmitHandler= event=>{
     event.preventDefault();
+    setEnteredNameTouched(true);
     const enteredValue=nameInputRef.current.value;
     if(enteredValue.trim()=== ''){
       setEnteredNameIsValid(false);
@@ -14,14 +22,15 @@ const SimpleInput = (props) => {
     setEnteredNameIsValid(true);
     console.log(enteredValue);
   }
-
-  const nameInputClasses= enteredNameIsValid ? 'form-control': 'form-control invalid';
+  
+  const nameInputIsInvalid=!enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses= nameInputIsInvalid ? 'form-control invalid': 'form-control';
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' />
-        {!enteredNameIsValid && <p className='error-text'>You did'nt enter any name,Please enter a name</p>}
+        {nameInputIsInvalid && <p className='error-text'>You did'nt enter any name,Please enter a name</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
