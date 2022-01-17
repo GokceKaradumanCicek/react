74b6205dc4846b5049074c47b6089482c0e5d24a
GokceKaradumanCicek <gokce@thestarware.com>
2022-01-17 16:02:18 +0300

    Creating anchor tag provide open corresponding URL,but reloading happened so state information is gone in this case

--- react-router-app/src/App.js
+++ react-router-app/src/App.js
@@ -1,15 +1,21 @@
 import {Route} from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Products from './components/Products';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 function App() {
   return (
     <div>
+      <header>
+        <MainHeader></MainHeader>
+      </header>
+      <main>
       <Route path="/welcome">
         <Welcome></Welcome>
       </Route>
       <Route path="/products">
         <Products></Products>
       </Route>
+      </main>
     </div>
   );
 }
